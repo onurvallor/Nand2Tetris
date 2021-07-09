c3003d0d04d08d5e7efdd3e621c5e1855131916c
Onur <onurvallor@gmail.com>
2021-07-08 22:03:42 -0400

    Initial commit

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,78 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+
+//1. Infinite Loop
+//2. Check if keyboard is pressed
+//3. draw black pixels on screen.
+//4. Keyboard release remove drawing.
+
+
+
+
+
+(LOOP)
+
+@SCREEN
+D=A
+@0
+M=D
+
+/////////////////////////
+(CHECK)
+@KBD
+D=M
+
+@BLACK
+D;JGT
+@WHITE
+D;JEQ
+
+@CHECK
+0;JMP
+////////////////////////
+(BLACK)
+@1
+M=-1
+@CHANGE
+0;JMP
+
+///////////////////////
+(WHITE)
+@1
+M=0
+@CHANGE
+0;JMP
+
+
+
+////////////////////
+(CHANGE)
+@1
+D=M
+
+@0
+A=M //SETS ADDRESS of Reg0 to M or Memory
+M=D //SET Reg0 Memory into a color
+
+@0
+D=M+1 //Inc to next pixel
+
+@KBD
+D=A-D //New RegD = Address - NEXT PIXEL
+
+@0
+M=M+1
+A=M
+
+@CHANGE
+D;JGT
+
+
+@LOOP
+0;JMP
+
+
+
+
+
